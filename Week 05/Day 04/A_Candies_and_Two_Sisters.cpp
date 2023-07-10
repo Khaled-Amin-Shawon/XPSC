83f3ab44c12eb00871e89d88691bc14a49c9fbba
BigKhaled <mdkhaledaminshawon@gmail.com>
2023-07-10 23:18:08 +0600

    first commit

---
+++ Week 05/Day 04/A_Candies_and_Two_Sisters.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+#define fi first
+#define se second
+
+const int N = 1010;
+
+using namespace std;
+
+int main()
+{
+        ios_base::sync_with_stdio(false);
+        int T;
+        cin >> T;
+        while(T--){
+                int n;
+                cin >> n;
+                cout << (n - 1) / 2 << "\n";
+        }
+}
